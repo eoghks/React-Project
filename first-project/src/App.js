56d7534fa467595f03b9f681a86543656397ac7f
eoghks <zhfldk7316@naver.com>
2024-05-05 13:11:49 +0900

    Chapter 4 Component Committed

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,11 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+
+//Component
+function Header() {
+  return (
+    <header>
+      <h1><a href="/">WEB</a></h1>
+    </header>
+  )
+}
+
+function Nav(){
+  return (
+    <nav>
+      <ol>
+        <li><a href="/read/1">html</a></li>
+        <li><a href="/read/2">javascript</a></li>
+        <li><a href="/read/3">css</a></li>
+      </ol>
+    </nav>
+  )
+}
+
+function Article(){
+  return (
+    <article>
+      <h2>Welcom</h2>
+      Hello, WEB
+    </article>
+  )
+}
 //className = class를 의미 ==> css사용
 function App() {
   return (
-    <div className="App">
-      Hello React!!
-    </div>
+    <div>
+      <Header />
+      <Nav />
+      <Article />
+      </div>
   );
 }
 
