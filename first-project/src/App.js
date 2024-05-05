e5f25059207d7998c83af9fdc4edee90fbd9bea6
eoghks <zhfldk7316@naver.com>
2024-05-05 13:27:33 +0900

    Chapter 5 props committed

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -2,41 +2,56 @@ import logo from './logo.svg';
 import './App.css';
 
 //Component
-function Header() {
+//props = argument
+function Header(props) {
   return (
     <header>
-      <h1><a href="/">WEB</a></h1>
+      <h1><a href="/">{props.title}</a></h1>
     </header>
-  )
+  )//{}=> var를 쓸때 필수
 }
 
-function Nav(){
+function Nav(props){
+  /*
+  const lis =[];
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }*/
+  //used map function
+  const list = props.topics.map((t) => {return <li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>})
+
   return (
     <nav>
       <ol>
-        <li><a href="/read/1">html</a></li>
-        <li><a href="/read/2">javascript</a></li>
-        <li><a href="/read/3">css</a></li>
+        {list}
       </ol>
     </nav>
   )
 }
 
-function Article(){
+function Article(props){
   return (
     <article>
-      <h2>Welcom</h2>
-      Hello, WEB
+      <h2>{props.title}</h2>
+      {props.body}
     </article>
   )
 }
 //className = class를 의미 ==> css사용
+//const => 다른 함수에서 수정 불가
 function App() {
+  const topics = [
+    {id: 1, title: 'html', body:"html is ..."},
+    {id: 2, title: 'javascript',  body:"javascript is ..."},
+    {id: 3, title: 'css', body:"css is ..."},
+  ];
+
   return (
     <div>
-      <Header />
-      <Nav />
-      <Article />
+      <Header title="REACT"/>
+      <Nav topics={topics}/>
+      <Article title="Welcom" body="Hello, React"/>
       </div>
   );
 }
